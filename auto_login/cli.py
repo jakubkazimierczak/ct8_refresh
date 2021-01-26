f887bd254a08aa08b8e30062d1ccb6090ff32496
jakubkazimierczak <kazimierczakjakub@outlook.com>
2021-01-26 23:55:36 +0100

    Changed help messages to be lower-case

--- auto_login/cli.py
+++ auto_login/cli.py
@@ -11,7 +11,7 @@ subparsers = parser.add_subparsers()
 parser.add_argument(
     '--debug',
     action='store_true',
-    help='Toggle debug mode on.'
+    help='toggle debug mode on'
 )
 
 
@@ -23,28 +23,28 @@ parser.add_argument(
     '-a', '--add-account',
     action='store',
     nargs='+',
-    help='Add account to DB.',
+    help='add account to DB',
     metavar='login',
 )
 parser.add_argument(
     '--del-account',
     action='store',
     nargs='+',
-    help='Delete account from DB.',
+    help='delete account from DB',
     metavar='login',
 )
 parser.add_argument(
     '-e', '--enable-account',
     action='store',
     nargs='+',
-    help='Enable account to automatic sign-in.',
+    help='enable account to automatic sign-in',
     metavar='login',
 )
 parser.add_argument(
     '-d', '--disable-account',
     action='store',
     nargs='+',
-    help='Disable account from automatic sign-in.',
+    help='disable account from automatic sign-in',
     metavar='login',
 )
 
