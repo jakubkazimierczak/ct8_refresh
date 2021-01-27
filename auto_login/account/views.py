b963fb59befb338c33442aeb571b891cc1afdd95
jakubkazimierczak <kazimierczakjakub@outlook.com>
2021-01-27 12:10:58 +0100

    Added styling to the users table view

--- auto_login/account/views.py
+++ auto_login/account/views.py
@@ -2,19 +2,39 @@ from auto_login.account.model import Account
 from auto_login.account.manager import AccountsManager
 from auto_login import console
 from rich.table import Table
+from datetime import datetime
 
 
 class AccountsView:
+    @staticmethod
+    def get_row_style(days_remaining):
+        if days_remaining < 30:
+            return 'red'
+        elif days_remaining < 60:
+            return 'yellow'
+        elif days_remaining < 90:
+            return 'green'
+        else:
+            return None
+
     @staticmethod
     def show_accounts():
         table = Table(title='Users')
         table.add_column('Name')
         table.add_column('Enabled')
         table.add_column('Expires on')
+        table.add_column('Remaining days')
 
-        accounts = AccountsManager.get_all_accounts().order_by(Account.is_active.desc())
+        accounts = AccountsManager.get_all_accounts().order_by(Account.is_active.desc(), Account.expires_on)
         account: Account
         for account in accounts:
-            table.add_row(account.name, str(account.is_active), account.expires_on)
+            # Calculate expiration date
+            expires_in, style = '', None
+            if account.expires_on:
+                expires_in = datetime.fromisoformat(str(account.expires_on)) - datetime.today()
+                style = AccountsView.get_row_style(expires_in.days)
+                expires_in = str(expires_in.days)
+
+            table.add_row(account.name, str(account.is_active), str(account.expires_on)[:-7], str(expires_in), style=style)
 
         console.print(table)
