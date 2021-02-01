c4fd7801df76f17b6c8f70c654812c1929af72bc
jakubkazimierczak <kazimierczakjakub@outlook.com>
2021-02-01 12:47:01 +0100

    Removed unused variables

--- ct8_refresh/run.py
+++ ct8_refresh/run.py
@@ -3,10 +3,7 @@ import sys
 from loguru import logger
 from pyppeteer import chromium_downloader
 from rich import print
-from rich.progress import (
-    BarColumn,
-    Progress
-)
+from rich.progress import BarColumn, Progress
 from rich.prompt import Confirm
 
 from . import console
@@ -17,7 +14,7 @@ from .ct8_pyppeteer import download_chromium
 
 
 class Run:
-    def __init__(self, all_users):
+    def __init__(self, all_users: bool):
         self.all_users = all_users
 
         self.progress = Progress(
@@ -28,36 +25,38 @@ class Run:
 
     @logger.catch
     def signin_loop(self):
-        progress = self.progress
-
-        with progress:
+        with self.progress:
             if self.all_users:
                 users = AccountsManager.get_all_accounts()
             else:
                 users = AccountsManager.get_active_accounts()
             if not users:
-                sys.exit('No active users found. Please configure your users first.')
+                sys.exit("No active users found. Please configure your users first.")
 
-            task_id = progress.add_task('signin_check', total=len(users))
-            progress.start_task(task_id)
+            task_id = self.progress.add_task("signin_check", total=len(users))
+            self.progress.start_task(task_id)
 
             user: Account
             for user in users:
-                progress.update(task_id, description=f'{user.name}: sign-in...')
+                self.progress.update(task_id, description=f"{user.name}: sign-in...")
 
-                ct8 = CT8(user.name, user.password, progress.console)
+                CT8(user.name, user.password, self.progress.console)
 
-                progress.advance(task_id)
+                self.progress.advance(task_id)
 
-            progress.update(task_id, description=':100: Completed!')
+            self.progress.update(task_id, description=":100: Completed!")
 
     @logger.catch
     def main(self):
         if not chromium_downloader.check_chromium():
-            print('A headless Chromium needs to be downloaded in order to continue.')
-            console.print("The download size is around [yellow]150MB[/yellow]. Proceed?")
-            if not Confirm.ask('Download and continue'):
-                sys.exit('Script terminated - headless Chromium is necessary to run the script.')
+            print("A headless Chromium needs to be downloaded in order to continue.")
+            console.print(
+                "The download size is around [yellow]150MB[/yellow]. Proceed?"
+            )
+            if not Confirm.ask("Download and continue"):
+                sys.exit(
+                    "Script terminated - headless Chromium is necessary to run the script."
+                )
 
             download_chromium()
 
