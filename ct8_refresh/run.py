891490b99892d6959eb9b2a48200ee4bd0b6dd7d
jakubkazimierczak <kazimierczakjakub@outlook.com>
2021-02-02 21:29:08 +0100

    Removed remnants of Chromium-related code

--- ct8_refresh/run.py
+++ ct8_refresh/run.py
@@ -1,16 +1,11 @@
 import sys
 
 from loguru import logger
-from pyppeteer import chromium_downloader
-from rich import print
 from rich.progress import BarColumn, Progress
-from rich.prompt import Confirm
 
-from . import console
 from .account.manager import AccountsManager
 from .account.model import Account
 from .ct8 import CT8
-from .ct8_pyppeteer import download_chromium
 
 
 class Run:
@@ -49,16 +44,4 @@ class Run:
 
     @logger.catch
     def main(self):
-        if not chromium_downloader.check_chromium():
-            print('A headless Chromium needs to be downloaded in order to continue.')
-            console.print(
-                'The download size is around [yellow]150MB[/yellow]. Proceed?'
-            )
-            if not Confirm.ask('Download and continue'):
-                sys.exit(
-                    'Script terminated - headless Chromium is necessary to run the script.'
-                )
-
-            download_chromium()
-
         self.signin_loop()
