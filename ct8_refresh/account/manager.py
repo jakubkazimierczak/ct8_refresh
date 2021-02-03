75eed521d914af7bd60eef280488bf00b5648b50
jakubkazimierczak <kazimierczakjakub@outlook.com>
2021-02-04 00:02:20 +0100

    Extracted try-except blocks into a decorator

--- ct8_refresh/account/manager.py
+++ ct8_refresh/account/manager.py
@@ -1,7 +1,7 @@
 from functools import partial
 
 from loguru import logger
-from peewee import OperationalError, IntegrityError
+from peewee import OperationalError, IntegrityError, DoesNotExist
 from rich.prompt import Prompt
 
 from ct8_refresh import console
@@ -12,74 +12,75 @@ print_warning = partial(console.print, style='yellow')
 print_error = partial(console.print, style='bright_red')
 
 
+def catcher(operation='Operation'):
+    def decorator(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, *kwargs)
+            except DoesNotExist as err:
+                logger.error(err)
+                print_error(f"{operation} failed: Account {args[0]} does not exists")
+            except (OperationalError, IntegrityError) as err:
+                logger.error(err)
+                print_error(f"{operation} failed: {err}")
+
+        return inner
+
+    return decorator
+
+
 class AccountsManager:
     @staticmethod
-    def _exists(account_name):
-        account = Account.select(Account.name).where(Account.name == account_name)
-        return list(account)
+    @catcher('Getting account')
+    def get_account(account_name):
+        return Account.get(Account.name == account_name)
 
     @staticmethod
-    def add(account_name):
-        if AccountsManager._exists(account_name):
+    @catcher('Add')
+    def add(account_name, password=None):
+        if AccountsManager.get_account(account_name):
             console.print(
                 f'Cannot add {account_name} - account exists in DB.', style='bright_red'
             )
             return
 
-        password = Prompt.ask(f'{account_name} password', password=True)
-        try:
-            Account.insert(name=account_name, password=password).execute()
-            print_success('Account added.')
-        except (OperationalError, IntegrityError) as err:
-            logger.error(err)
-            console.print(f'Failed to add user: {err}', style='bold red')
+        if not (password := password):
+            password = Prompt.ask(f'{account_name} password', password=True)
+        Account.create(name=account_name, password=password)
+        print_success('Account added.')
 
     @staticmethod
+    @catcher('Delete')
     def delete(account_name):
-        try:
-            deleted = Account.delete().where(Account.name == account_name).execute()
-            if deleted:
-                print_success('Account deleted')
-            else:
-                print_warning('Account not found.')
-        except (OperationalError, IntegrityError) as err:
-            logger.error(err)
-            console.print(f'Failed to remove user: {err}', style='bold red')
+        deleted = Account.delete().where(Account.name == account_name).execute()
+        if deleted:
+            print_success(f'Account {account_name} deleted')
+        else:
+            print_warning(f'Delete failed - account {account_name} not found.')
 
     @staticmethod
+    @catcher('Changing active status')
     def set_active(account_name, value: bool):
-        operation = 'enable' if value else 'disable'
+        operation = 'enabled' if value else 'disabled'
 
-        try:
-            Account.update({Account.is_active: value}).where(
-                Account.name == account_name
-            ).execute()
-            print_success(f'Account {operation}d (automatic sign-in {operation}d).')
-        except (OperationalError, IntegrityError) as err:
-            logger.error(err)
-            console.print(f'Failed to {operation} account: {err}', style='bold red')
+        account = AccountsManager.get_account(account_name)
+        account.is_active = value
+        account.save()
+        print_success(f'Account {operation} (automatic sign-in {operation}d).')
 
     @staticmethod
+    @catcher('Updating expiration date')
     def update_expiration_date(account_name, date):
-        try:
-            Account.update({Account.expires_on: date}).where(
-                Account.name == account_name
-            ).execute()
-            # print_success(f'Account {operation}d (automatic sign-in {operation}d).')
-        except (OperationalError, IntegrityError) as err:
-            logger.error(err)
-            # console.print(f'Failed to {operation} account: {err}', style='bold red')
+        account = AccountsManager.get_account(account_name)
+        account.expires_on = date
+        account.save()
 
     @staticmethod
+    @catcher('Getting all accounts')
     def get_all_accounts():
         return Account.select()
 
     @staticmethod
+    @catcher('Getting all active accounts')
     def get_active_accounts():
         return Account.select().where(Account.is_active)
-
-
-if __name__ == '__main__':
-    acc: Account
-    for acc in AccountsManager.get_active_accounts():
-        print(acc.name)
