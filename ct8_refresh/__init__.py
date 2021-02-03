8e1df4f9ea97ce4be923a168ac8578667f83ca6e
jakubkazimierczak <kazimierczakjakub@outlook.com>
2021-02-03 11:54:04 +0100

    Added Final type to module-level constants

--- ct8_refresh/__init__.py
+++ ct8_refresh/__init__.py
@@ -1,10 +1,11 @@
 __version__ = '0.1.0'
 
 from pathlib import Path
+from typing import Final
 
 from rich.console import Console
 
-ROOT_DIR = Path(__file__).parent.parent
-DATABASE_PATH = ROOT_DIR / 'accounts.db'
-LOG_PATH = ROOT_DIR / 'run.log'
-console = Console()
+ROOT_DIR: Final = Path(__file__).parent.parent
+DATABASE_PATH: Final = ROOT_DIR / 'accounts.db'
+LOG_PATH: Final = ROOT_DIR / 'run.log'
+console: Final = Console()
